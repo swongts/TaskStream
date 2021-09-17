d7d06cd8e17a83f09d13649a8676281e6cbd5dec
swongts <70316271+swongts@users.noreply.github.com>
2021-09-17 17:39:02 +0800

    Add filter by streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,11 +16,12 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
-
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+//        System.out.println("Printing deadlines");
+//        printDeadlines(tasksData);
+//
+//        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        printDeadlinesUsingStream(tasksData);
+        ArrayList<Task> filteredList = filterTasksByString(tasksData, "11");
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -46,4 +47,19 @@ public class Main {
             }
         }
     }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterTasksByString(ArrayList<Task> tasks, String string) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream()
+                .filter(t -> t.getDescription().contains(string))
+                .collect(Collectors.toList());
+        return filteredList;
+    }
 }
+
